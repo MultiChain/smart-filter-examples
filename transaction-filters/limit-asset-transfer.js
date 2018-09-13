4968f70a5c2fdc6f31b3a1bc2087c66c77abb324
Gideon Greenspan <github@gidgreen.com>
2018-09-13 15:05:14 +0300

    First example filters

---
+++ transaction-filters/limit-asset-transfer.js
@@ -0,0 +1,41 @@
+/* Only allow 1000 raw units of asset named USD to be sent to any address per transaction */
+/* It is recommended to perform asset quantity calculations using raw units for perfect precision */
+
+function filtertransaction()
+{
+	var tx=getfiltertransaction();
+	var addressbalancechange={};
+	
+	for (var inputnum=0; inputnum<tx.vin.length; inputnum++)
+		outputtoaddressbalancechange(getfiltertxinput(inputnum), true, addressbalancechange);
+		
+	for (var outputnum=0; outputnum<tx.vout.length; outputnum++)
+		outputtoaddressbalancechange(tx.vout[outputnum], false, addressbalancechange);
+	
+	for (var address in addressbalancechange)
+		if (addressbalancechange[address]>1000)
+			return "Address "+address+" is receiving too much USD";
+}
+
+function outputtoaddressbalancechange(output, spent, addressbalancechange)
+{
+	if (output.assets)
+	{
+		for (var assetnum=0; assetnum<output.assets.length; assetnum++)
+		{
+			if (output.assets[assetnum].name=="USD")
+			{
+				for (addressnum=0; addressnum<output.scriptPubKey.addresses.length; addressnum++)
+				{
+					var address=output.scriptPubKey.addresses[addressnum];
+					
+					if (!addressbalancechange.hasOwnProperty(address))
+						addressbalancechange[address]=0;
+					
+					var rawchange=spent ? -output.assets[assetnum].raw : output.assets[assetnum].raw;
+					addressbalancechange[address]+=rawchange;
+				}
+			}
+		}
+	}
+}
