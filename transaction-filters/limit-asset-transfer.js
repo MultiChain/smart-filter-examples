3ad0097a15fd4934f77e668e69ca94c9e3d24369
Gideon Greenspan <github@gidgreen.com>
2018-11-05 15:52:03 +0200

    Updated for 2.0 alpha 6, including stream filters

--- transaction-filters/limit-asset-transfer.js
+++ transaction-filters/limit-asset-transfer.js
@@ -8,31 +8,4 @@ function filtertransaction()
 	for (var address in addressbalancechange)
 		if (addressbalancechange[address]>1000)
 			return "Address "+address+" is receiving too much USD";
-<<<<<<< HEAD
 }
-=======
-}
-
-function outputtoaddressbalancechange(output, spent, addressbalancechange)
-{
-	if (output && output.assets)
-	{
-		for (var assetnum=0; assetnum<output.assets.length; assetnum++)
-		{
-			if (output.assets[assetnum].name=="USD")
-			{
-				for (var addressnum=0; addressnum<output.scriptPubKey.addresses.length; addressnum++)
-				{
-					var address=output.scriptPubKey.addresses[addressnum];
-					
-					if (!addressbalancechange.hasOwnProperty(address))
-						addressbalancechange[address]=0;
-					
-					var rawchange=spent ? -output.assets[assetnum].raw : output.assets[assetnum].raw;
-					addressbalancechange[address]+=rawchange;
-				}
-			}
-		}
-	}
-}
->>>>>>> b3387f760f4cabab6a1da73fe2a8b709663efe37
